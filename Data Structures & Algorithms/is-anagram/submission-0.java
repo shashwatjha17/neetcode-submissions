cd782e5ae8e6f38559fff69dd8095423ab3d2e1e
Shashwat Jha <141913631+shashwatjha17@users.noreply.github.com>
2026-09-06 23:08:01 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch:s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch : t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)-1);
+        }
+        for(int n : map.values()){
+            if(n!=0) return false;
+        }
+        return true;
+
+    }
+}
